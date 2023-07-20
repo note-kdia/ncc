37c0856ef5d62fc4ffe3e409e9013176713abc2f
note-kdia <naruse147@gmail.com>
2023-07-21 00:55:20 +0900

    Implement AST

--- ncc.c
+++ ncc.c
@@ -23,6 +23,24 @@ struct Token
     char *str;      // Token string
 };
 
+typedef enum
+{
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_NUM, // integer
+} NodeKind;
+
+typedef struct Node Node;
+
+// Node type of Abstract Syntax
+struct Node
+{
+    NodeKind kind; // Node kind
+    Node *lhs;     // left hand side
+    Node *rhs;     // right hand side
+    int val;       // If kind is ND_NUM, its value
+};
+
 // Input program
 char *user_input;
 
@@ -135,6 +153,71 @@ Token *tokenize()
     return head.next;
 }
 
+//
+// Parser
+//
+
+// expr = num ("+" num | "-" num)*
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr()
+{
+    Node *node = new_node_num(expect_number());
+
+    for (;;)
+    {
+        if (consume('+'))
+            node = new_node(ND_ADD, node, expr());
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, expr());
+        else
+            return node;
+    }
+}
+
+void gen(Node *node)
+{
+    if (node->kind == ND_NUM)
+    {
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+
+    switch (node->kind)
+    {
+    case ND_ADD:
+        printf("    add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("    sub rax, rdi\n");
+        break;
+    }
+
+    printf("    push rax\n");
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 2)
@@ -145,27 +228,18 @@ int main(int argc, char **argv)
 
     user_input = argv[1];
     token = tokenize();
+    Node *node = expr();
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    // The first token must be a number.
-    printf("    mov rax, %d\n", expect_number());
+    // Traverse the AST to emit assembly.
+    gen(node);
 
-    // ... followed by either `+ <number>` or `- <number>`.
-    while (!is_at_eof())
-    {
-        if (consume('+'))
-        {
-            printf("    add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("    sub rax, %d\n", expect_number());
-    }
-
+    // A result must be at the top of the stack, so pop it
+    // to RAX to make it a program exit code.
+    printf("    pop rax\n");
     printf("    ret\n");
     return 0;
 }
