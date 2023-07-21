ed2507cd9cca16bdf7a1e1b8f6c8513763596f43
Daiki Naruse <naruse147@gmail.com>
2023-07-21 11:08:25 +0900

    Add / and ()

--- ncc.c
+++ ncc.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+const char *PUNCTUATORS = "+-*/()";
+
 typedef enum
 {
     TK_RESERVED, // Keywords or punctuator
@@ -28,6 +30,7 @@ typedef enum
     ND_ADD, // +
     ND_SUB, // -
     ND_MUL, // *
+	ND_DIV, // /
     ND_NUM, // integer
 } NodeKind;
 
@@ -133,7 +136,7 @@ Token *tokenize()
         }
 
         // Punctuator
-        if (*p == '+' || *p == '-' || *p == '*')
+        if (strchr(PUNCTUATORS, *p))
         {
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
@@ -158,9 +161,6 @@ Token *tokenize()
 // Parser
 //
 
-// expr = mul ("+" mul | "-" mul)*
-// expr = num ("*" mul)*
-
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
 {
     Node *node = calloc(1, sizeof(Node));
@@ -178,18 +178,13 @@ Node *new_node_num(int val)
     return node;
 }
 
-Node *mul()
-{
-    Node *node = new_node_num(expect_number());
+// expr 	= mul ("+" mul | "-" mul)*
+// mul		= primary ("*" primary | "/" primary)*
+// primary  = num | "(" expr ")"
 
-    for (;;)
-    {
-        if (consume('*'))
-            node = new_node(ND_MUL, node, mul());
-        else
-            return node;
-    }
-}
+Node *expr();
+Node *mul();
+Node *primary();
 
 Node *expr()
 {
@@ -201,11 +196,36 @@ Node *expr()
             node = new_node(ND_ADD, node, mul());
         else if (consume('-'))
             node = new_node(ND_SUB, node, mul());
-        else if (consume('*'))
-            node = new_node(ND_MUL, node, mul());
         else
             return node;
     }
+}
+
+Node *mul()
+{
+    Node *node = primary();
+
+    for (;;)
+    {
+        if (consume('*'))
+            node = new_node(ND_MUL, node, primary());
+		else if (consume('/'))
+			node = new_node(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+Node *primary()
+{
+    if (consume('('))
+    {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    return new_node_num(expect_number());
 }
 
 void gen(Node *node)
@@ -233,6 +253,10 @@ void gen(Node *node)
     case ND_MUL:
         printf("    imul rax, rdi\n");
         break;
+	case ND_DIV:
+		printf("    cqo\n");
+		printf("    idiv rdi\n");
+		break;
     }
 
     printf("    push rax\n");
--- test.sh
+++ test.sh
@@ -21,5 +21,11 @@ assert 42 42
 assert 21 '5+20-4'
 assert 41 ' 12 + 34 - 5 '
 assert 18 '3 * 6'
+assert 3 '21 / 7'
+assert 15 '3+6*2'
+assert 5 '14-6/2*3'
+assert 23 '6/2*3+2*7'
+assert 18 '3 *(  2+4 )'
+assert 5 '70/(2+4*3)'
 
 echo OK
